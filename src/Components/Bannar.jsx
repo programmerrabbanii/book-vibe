2e602d4d0a546041598cdc38020af461bc9b8292
programmerrabbanii <developerrabbanisarker@gmail.com>
2025-03-05 13:31:01 +0600

    added the bannar section here

--- src/Components/Bannar.jsx
+++ src/Components/Bannar.jsx
@@ -1,14 +1,23 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import React from "react";
+import bannarImg from "../assets/books.jpg"
 
-function Bannar() {
+const Bannar = () => {
   return (
-    <Swiper spaceBetween={50} slidesPerView={1} autoplay={{ delay: 2000 }} loop>
-      <SwiperSlide><img src="image1.jpg" alt="Slide 1" /></SwiperSlide>
-      <SwiperSlide><img src="image2.jpg" alt="Slide 2" /></SwiperSlide>
-      <SwiperSlide><img src="image3.jpg" alt="Slide 3" /></SwiperSlide>
-    </Swiper>
+    <div>
+      <div className="hero  h-[80%] w-11/12 mx-auto rounded-lg mt-5">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img 
+            src={bannarImg}
+            className=" rounded-lg shadow-2xl"
+          />
+          <div> 
+            <h1 className="text-5xl font-bold">Books to freshen up <br />your bookshelf</h1>
+            <button className="btn btn-primary bg-[#23BE0A] mt-8">View The List</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Bannar;
